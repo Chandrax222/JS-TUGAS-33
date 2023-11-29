c88a0fc24fa1b4ddc4a7fc7f14376d9445ee45e6
Chandra Wiratama Putera <141868663+Chandrax222@users.noreply.github.com>
2023-11-29 23:41:48 +0700

    Meanggil atau membuat element class
    Meanggil atau membuat element class

---
+++ Tugas33.js
@@ -0,0 +1,14 @@
+function Tugas33() {
+
+      // const x = ['tugas','dari','dia'];
+
+      // for (let i = 0; i < x.length; i++) {
+      //       const element = x[i];
+
+      //       console.log(element);
+            
+      // }
+
+      console.log(document.getElementsByClassName('tugas'));
+}
+Tugas33();
